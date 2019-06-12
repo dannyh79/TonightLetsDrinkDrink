cb2c0cd140c2918e0a2756c3ccb81c0f812c8de1
danny-ch han <chenghsuan.han@gmail.com>
2019-06-12 12:01:37 +0800

    1. Added model "drink" and some placeholder data in seeds.rb 2. Route for calc page and a temp controller for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # calc_page
+  resource :calcs, only: [:show]  # 寫 helper 帶資料進去
+
+
 end
+
