0b6375595821aa0331b50d7f751095f9325996ad
mkx666 <m.k.chen.1021@gmail.com>
2019-06-14 17:10:41 +0800

    fix_migrate_space

--- db/migrate/20190614073235_create_user_defines.rb
+++ db/migrate/20190614073235_create_user_defines.rb
@@ -2,10 +2,10 @@ class CreateUserDefines < ActiveRecord::Migration[5.2]
   def change
     create_table :user_defines do |t|
       t.references :user, foreign_key: true
-      t.string :name, null:false, default: ''
-      t.string :drink_id, null:false, array:true, default: []
-      t.string :ratio, null:false, array:true, default: []
-      t.string :ingredient_volume_alcohol, null:false, array:true, default: []
+      t.string :name, null: false, default: ''
+      t.string :drink_id, null: false, array: true, default: []
+      t.string :ratio, null: false, array: true, default: []
+      t.string :ingredient_volume_alcohol, null: false, array: true, default: []
 
       t.timestamps
     end
