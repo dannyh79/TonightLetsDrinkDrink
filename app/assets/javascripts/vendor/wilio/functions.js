b6329e636781cfddc128aebdffc45bec7269e9c7
mkx666 <m.k.chen.1021@gmail.com>
2019-06-14 20:33:23 +0800

    fix functions.js 的 turbolinks load 問題

--- app/assets/javascripts/vendor/wilio/functions.js
+++ app/assets/javascripts/vendor/wilio/functions.js
@@ -3,7 +3,7 @@
 	"use strict";
 	
 	// Preload
-	$(window).on('load', function () { // makes sure the whole site is loaded
+	$(document).on('turbolinks:load', function () { // makes sure the whole site is loaded
 		$('[data-loader="circle-side"]').fadeOut(); // will first fade out the loading animation
 		$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
 		$('body').delay(350).css({
