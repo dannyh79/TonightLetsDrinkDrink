d1f0d70c99754091ff286713b7dfe9901334c38b
mkx666 <m.k.chen.1021@gmail.com>
2019-06-14 13:58:07 +0800

    修正 migrate weight default 0 -> 1, database.yml delete the username and password

--- db/migrate/20190612042242_devise_create_users.rb
+++ db/migrate/20190612042242_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
       t.boolean :gender,            null: false, default: false
-      t.decimal :weight,            null: false, default: 0
+      t.decimal :weight,            null: false, default: 1
 
       ## Recoverable
       t.string   :reset_password_token
