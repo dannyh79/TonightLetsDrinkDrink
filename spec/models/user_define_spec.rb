6c66b803081f47c36bb68e673fd9415ebd82fbf0
danny-ch han <chenghsuan.han@gmail.com>
2019-06-15 09:23:05 +0800

    Issue: UserDefine's instance can NOT be created if the class is inherited from ApplicationRecord (see model "user_define.rb")

--- spec/models/user_define_spec.rb
+++ spec/models/user_define_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UserDefine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "total concentration derived from ratio must match the expected" do
+    ud1 = UserDefine.new('good cock-tail', [2, 4], [3, 1], [0, 40])
+    ud2 = UserDefine.new('very good cock-tail', [2, 4, 1], [3, 1, 6], [0, 40, 4.5])
+
+    expect(ud1.volume_alcohol).to eq 10
+    expect(ud2.volume_alcohol).to eq 6.7
+  end
 end
