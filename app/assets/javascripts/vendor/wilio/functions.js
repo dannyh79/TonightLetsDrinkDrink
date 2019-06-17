c6f9da93dbbce76ce48ba845ae6b58dc961c6d48
CHAO CHAO WU <carion0216@gmail.com>
2019-06-17 17:15:42 +0800

    fixed nav_bar action

--- app/assets/javascripts/vendor/wilio/functions.js
+++ app/assets/javascripts/vendor/wilio/functions.js
@@ -5,133 +5,137 @@ $(document).on('turbolinks:load', function () { // makes sure the whole site is 
 	$('body').delay(350).css({
 		'overflow': 'visible'
 	});
+	init_event();
 })
 
-// Submit loader mask 
-$('form#wrapped').on('submit', function () {
-	var form = $("form#wrapped");
-	form.validate();
-	if (form.valid()) {
-		$("#loader_form").fadeIn();
-	}
-});
-
-// Jquery select
-$('.styled-select select').niceSelect();
-
-// Show Password
-$('#password1, #password2').hidePassword('focus', {
-	toggle: {
-		className: 'my-toggle'
-	}
-});
-
-// Range slider
-$('input[type="range"]').rangeslider({
-	polyfill: false,
-	onInit: function () {
-		this.output = $(".budget_slider span").html(this.$element.val());
-	},
-	onSlide: function (
-		position, value) {
-		this.output.html(value);
-	}
-});
-
-// Button start scroll to section
-	$('a[href^="#"].mobile_btn').on('click', function (e) {
-		e.preventDefault();
-		var target = this.hash;
-		var $target = $(target);
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top
-		}, 400, 'swing', function () {
-			window.location.hash = target;
-		});
+function init_event() {
+	// Submit loader mask 
+	$('form#wrapped').on('submit', function () {
+		var form = $("form#wrapped");
+		form.validate();
+		if (form.valid()) {
+			$("#loader_form").fadeIn();
+		}
 	});
 
-// Menu
-var overlayNav = $('.cd-overlay-nav'),
-	overlayContent = $('.cd-overlay-content'),
-	navigation = $('.cd-primary-nav'),
-	toggleNav = $('.cd-nav-trigger');
+	// Jquery select
+	$('.styled-select select').niceSelect();
 
-//inizialize navigation and content layers
-layerInit();
-$(window).on('resize', function(){
-	window.requestAnimationFrame(layerInit);
-});
+	// Show Password
+	$('#password1, #password2').hidePassword('focus', {
+		toggle: {
+			className: 'my-toggle'
+		}
+	});
 
-//open/close the menu and cover layers
-toggleNav.on('click', function(){
-	if(!toggleNav.hasClass('close-nav')) {
-		//it means navigation is not visible yet - open it and animate navigation layer
-		toggleNav.addClass('close-nav');
-		
-		overlayNav.children('span').velocity({
-			translateZ: 0,
-			scaleX: 1,
-			scaleY: 1,
-		}, 500, 'easeInCubic', function(){
-			navigation.addClass('fade-in');
+	// Range slider
+	$('input[type="range"]').rangeslider({
+		polyfill: false,
+		onInit: function () {
+			this.output = $(".budget_slider span").html(this.$element.val());
+		},
+		onSlide: function (
+			position, value) {
+			this.output.html(value);
+		}
+	});
+
+	// Button start scroll to section
+		$('a[href^="#"].mobile_btn').on('click', function (e) {
+			e.preventDefault();
+			var target = this.hash;
+			var $target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 400, 'swing', function () {
+				window.location.hash = target;
+			});
 		});
-	} else {
-		//navigation is open - close it and remove navigation layer
-		toggleNav.removeClass('close-nav');
-		
-		overlayContent.children('span').velocity({
-			translateZ: 0,
-			scaleX: 1,
-			scaleY: 1,
-		}, 500, 'easeInCubic', function(){
-			navigation.removeClass('fade-in');
-			
+
+	// Menu
+	var overlayNav = $('.cd-overlay-nav'),
+		overlayContent = $('.cd-overlay-content'),
+		navigation = $('.cd-primary-nav'),
+		toggleNav = $('.cd-nav-trigger');
+
+	//inizialize navigation and content layers
+	layerInit();
+	$(window).on('resize', function(){
+		window.requestAnimationFrame(layerInit);
+	});
+
+	//open/close the menu and cover layers
+	toggleNav.on('click', function(e){
+		e.preventDefault();
+		if(!toggleNav.hasClass('close-nav')) {
+			//it means navigation is not visible yet - open it and animate navigation layer
+			toggleNav.addClass('close-nav');
+
 			overlayNav.children('span').velocity({
 				translateZ: 0,
-				scaleX: 0,
-				scaleY: 0,
-			}, 0);
-			
-			overlayContent.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				overlayContent.children('span').velocity({
-					translateZ: 0,
-					scaleX: 0,
-					scaleY: 0,
-				}, 0, function(){overlayContent.removeClass('is-hidden')});
+				scaleX: 1,
+				scaleY: 1,
+			}, 500, 'easeInCubic', function(){
+				navigation.addClass('fade-in');
 			});
-			if($('html').hasClass('no-csstransitions')) {
-				overlayContent.children('span').velocity({
+		} else {
+			//navigation is open - close it and remove navigation layer
+			toggleNav.removeClass('close-nav');
+			
+			overlayContent.children('span').velocity({
+				translateZ: 0,
+				scaleX: 1,
+				scaleY: 1,
+			}, 500, 'easeInCubic', function(){
+				navigation.removeClass('fade-in');
+				
+				overlayNav.children('span').velocity({
 					translateZ: 0,
 					scaleX: 0,
 					scaleY: 0,
-				}, 0, function(){overlayContent.removeClass('is-hidden')});
-			}
-		});
+				}, 0);
+				
+				overlayContent.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					overlayContent.children('span').velocity({
+						translateZ: 0,
+						scaleX: 0,
+						scaleY: 0,
+					}, 0, function(){overlayContent.removeClass('is-hidden')});
+				});
+				if($('html').hasClass('no-csstransitions')) {
+					overlayContent.children('span').velocity({
+						translateZ: 0,
+						scaleX: 0,
+						scaleY: 0,
+					}, 0, function(){overlayContent.removeClass('is-hidden')});
+				}
+			});
+		}
+	});
+
+	function layerInit(){
+		var diameterValue = (Math.sqrt( Math.pow($(window).height(), 2) + Math.pow($(window).width(), 2))*2);
+		overlayNav.children('span').velocity({
+			scaleX: 0,
+			scaleY: 0,
+			translateZ: 0,
+		}, 50).velocity({
+			height : diameterValue+'px',
+			width : diameterValue+'px',
+			top : -(diameterValue/2)+'px',
+			left : -(diameterValue/2)+'px',
+		}, 0);
+
+		overlayContent.children('span').velocity({
+			scaleX: 0,
+			scaleY: 0,
+			translateZ: 0,
+		}, 50).velocity({
+			height : diameterValue+'px',
+			width : diameterValue+'px',
+			top : -(diameterValue/2)+'px',
+			left : -(diameterValue/2)+'px',
+		}, 0);
 	}
-});
 
-function layerInit(){
-	var diameterValue = (Math.sqrt( Math.pow($(window).height(), 2) + Math.pow($(window).width(), 2))*2);
-	overlayNav.children('span').velocity({
-		scaleX: 0,
-		scaleY: 0,
-		translateZ: 0,
-	}, 50).velocity({
-		height : diameterValue+'px',
-		width : diameterValue+'px',
-		top : -(diameterValue/2)+'px',
-		left : -(diameterValue/2)+'px',
-	}, 0);
-
-	overlayContent.children('span').velocity({
-		scaleX: 0,
-		scaleY: 0,
-		translateZ: 0,
-	}, 50).velocity({
-		height : diameterValue+'px',
-		width : diameterValue+'px',
-		top : -(diameterValue/2)+'px',
-		left : -(diameterValue/2)+'px',
-	}, 0);
 }
-
