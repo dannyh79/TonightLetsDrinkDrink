65705b25cdf480c0b94337552afb9c5776da4000
danny-ch han <chenghsuan.han@gmail.com>
2019-06-17 11:58:40 +0800

    1. Add column "display_name" to table "drinks"; 2. Update seeds.rb according to 1. 3. "calc page": Changed booze name's resource from "name" to "display_name" in the form and the result

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_042242) do
+ActiveRecord::Schema.define(version: 2019_06_17_034825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2019_06_12_042242) do
     t.string "img_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "display_name"
+  end
+
+  create_table "user_defines", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name", default: "", null: false
+    t.string "drink_id", default: [], null: false, array: true
+    t.string "ratio", default: [], null: false, array: true
+    t.string "ingredient_volume_alcohol", default: [], null: false, array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_defines_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +49,5 @@ ActiveRecord::Schema.define(version: 2019_06_12_042242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_defines", "users"
 end
