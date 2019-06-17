8f528d446fb1ef42948d6b65f7ecb52cad996bc6
Eva Yang <evayangms@gmail.com>
2019-06-17 14:31:57 +0800

    reset password email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Eva <evayangms@gmail.com>' #改成你想顯示的寄件者
+  config.mailer_sender = 'TonightLetsDrinkDrink <tonightlets@drinkdrink.com>' #改成你想顯示的寄件者
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
