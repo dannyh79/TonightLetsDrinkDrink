b32a7cad0c219fdd5992b0fb0c615124869df91b
danny-ch han <chenghsuan.han@gmail.com>
2019-06-19 20:54:06 +0800

    Merge 'develop' into 'vendor-layout'; set env to production-ready

--- db/schema.rb
+++ db/schema.rb
@@ -35,17 +35,6 @@ ActiveRecord::Schema.define(version: 2019_06_17_034825) do
     t.index ["user_id"], name: "index_user_defines_on_user_id"
   end
 
-  create_table "user_defines", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "name", default: "", null: false
-    t.string "drink_id", default: [], null: false, array: true
-    t.string "ratio", default: [], null: false, array: true
-    t.string "ingredient_volume_alcohol", default: [], null: false, array: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_defines_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
