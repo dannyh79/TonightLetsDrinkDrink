08e8720c46766afd84bc77d83c17e6c3c6f0c8f4
MK <48974903+mkx777@users.noreply.github.com>
2019-06-20 15:47:46 +0800

    fix error log in calc (#31)

--- app/assets/javascripts/frontend/calc.js
+++ app/assets/javascripts/frontend/calc.js
@@ -8,7 +8,6 @@ $(document).on('turbolinks:load', function () {
 
   let liquorName = "未知";
   let weight = 0;
-  let gender = 0;
   let timeBefore = 0;
   let timeAfter = 0;
   let alcoholContent = 0; // 酒品的酒精濃度
@@ -21,13 +20,18 @@ $(document).on('turbolinks:load', function () {
   let genderConst = 0;
   let rightPage = true;
 
+  // 設定性別函式
   function setGenderConst(gender) {
     return (gender === 0 ? menConst : womenConst)
   }
 
+  // 計算杯數函式
   function calcDrink(bac) {
+    // 設定性別
     genderConst = setGenderConst(gon.current_user_gender);
+    // 設定體重
     weight = gon.current_user_weight;
+    // 計算杯數
     if ((bac !== 0) && (genderConst !== 0) && (weight !== 0) && (alcoholContent !== 0)) {
       return (100 * weight * toLB * genderConst * (bac + 0.00025 * (timeBefore + timeAfter))) / (5.14 * unit * alcoholContent * toOunces)
     } else {
@@ -36,7 +40,9 @@ $(document).on('turbolinks:load', function () {
     }
   }
 
+  // 計算達到酒測標準時間函式
   function calcDrive(drinksLevel) {
+    // 計算式
     if ((genderConst !== 0) && (weight !== 0) && (alcoholContent !== 0)) {
       return (((205.6 * drinksLevel * unit * alcoholContent * toOunces) / (weight * toLB * genderConst))) - (timeBefore + timeAfter) - (4000 * bacLevel[0]);
     } else {
@@ -45,6 +51,7 @@ $(document).on('turbolinks:load', function () {
     }
   }
 
+  // 分鐘轉時間顯示
   function minToTime(minutes) {
     let hours = 0;
     hours = Math.floor(minutes / 60);
@@ -52,6 +59,7 @@ $(document).on('turbolinks:load', function () {
     return `${hours} 小時 ${minutes} 分`
   }
 
+  // 設定 modal 內容
   function setModal(id) {
     // 取得 levelnumber 的 number(最後一個字)
     id = id[id.length - 1];
@@ -59,78 +67,83 @@ $(document).on('turbolinks:load', function () {
     $(`#modal-bac`).text(`血酒濃度：${bacLevel[id - 1]} %`);
     $(`.modal-body .info .level${id}`).show();
     $(`#after-drink`).text(driveText[id - 1]);
-    // console.log(driveText[id -1]);
   }
 
   // 變更層級按鈕
   function changeButton() {
+    // 分次計算不同程度所需的杯數並輸入進 array
     for (let i = 0; i < 5; i++) {
       maxDrinksLevel[i] = Math.floor((calcDrink(bacLevel[i])) * 10) / 10;
     }
 
+    // 設定各杯數文字內容
     levelText[0] = `（正常）：${liquorName} ${maxDrinksLevel[0]} 杯`;
     levelText[1] = `心情愉悅：${liquorName} ${maxDrinksLevel[1]} 杯`;
     levelText[2] = `亢奮：${liquorName} ${maxDrinksLevel[2]} 杯`;
     levelText[3] = `噁心想吐：${liquorName} ${maxDrinksLevel[3]} 杯`;
     levelText[4] = `「斷片」：${liquorName} ${maxDrinksLevel[4]} 杯`;
 
+    // 轉換杯數文字 array 進 html 內
     for (let i = 0; i < 5; i++) {
       $(`#level${i+1}`).text(levelText[i]);
     }
 
-
+    // 設定達到酒測值的文字 array
     for (let i = 0; i < 5; i++) {
       safeDrive[i] = Math.ceil(calcDrive(maxDrinksLevel[i]));
     }
 
+    // 轉換酒測標準文字 array 進 html
     for (let i = 0; i < 5; i++) {
       if (safeDrive[i] <= 0) {
         driveText[i] = `血酒濃度低於酒測標準 ${bacLevel[0]} %`;
-        // console.log(driveText[i]);
       } else {
         driveText[i] = `結束後 ${minToTime(safeDrive[i])}，血酒濃度可低於酒測標準 ${bacLevel[0]} %`;
-        // console.log(driveText[i]);
       }
     }
   }
 
+  // 事件
   // 選擇酒類
   $('#calc-drinks input[type=radio]').click(function (e) {
+    // 抓取飲品酒精濃度並轉為數字
     alcoholContent = $('#calc-drinks input[type=radio]:checked').val();
     alcoholContent = Number(alcoholContent);
+    // 抓取飲品名字
     liquorName = $(this).attr('data');
-    console.log(liquorName)
-    // $('#calcdrinks .form-grop input').removeClass('for-checked-form-group');
+    // 設定選取選項的外觀
     $('#calc-drinks label').removeClass('for-checked-form-group');
     $(this).parent('label').addClass('for-checked-form-group');
-    // console.log(this);    
     changeButton();
   })
+
+  // 選單事件
+  // 選擇開始時間
   $('select[name="time-before"]')
     .change(function () {
       timeBefore = $('select[name="time-before"] option:selected').val();
       timeBefore = Number(timeBefore);
-      // console.log(timeBefore);
-      // changeButton();
     })
+
+  // 選擇結束時間
   $('select[name="time-after"]')
     .change(function () {
       timeAfter = $('select[name="time-after"] option:selected').val();
       timeAfter = Number(timeAfter);
-      // console.log(timeAfter);
-      // changeButton();
     })
+
+  // 選擇單位
   $('select[name="unit"]')
     .change(function () {
       unit = $('select[name = "unit"] option:selected').val();
       unit = Number(unit);
-      // console.log(unit);
-      // changeButton();
     })
 
 
   // 按鈕事件
+  // 下一步
   $("#next-step").click(function () {
+    // 如果第一次按要展開 如果第二次就不會再展開(時間選擇區(第二區塊))
     if (alcoholContent !== 0  && rightPage) {
       $("#start").removeClass("d-none");
       rightPage = false;
@@ -139,12 +152,16 @@ $(document).on('turbolinks:load', function () {
       $(window).scrollTop(10000);
     }
   });
+
+  // 顯示計算結果並捲動到底部
   $("#to-result").click(function () {
     $("#time-unit").addClass("d-none");
     $("#result-step").removeClass("d-none");
     $(window).scrollTop(10000);
     changeButton();
   });
+
+  // 回到選酒頁並捲到到頂部
   $("#to-time-unit").click(function () {
     $("#result-step").addClass("d-none");
     $("#time-unit").removeClass("d-none");
@@ -152,28 +169,15 @@ $(document).on('turbolinks:load', function () {
     changeButton();
   });
 
-
+  // 綁定杯數按鈕事件
   $('#result button').click(function (e) {
+    // 隱藏 modal 內部對身體影響的資料
     $(`.modal-body .info li`).hide();
     setModal($(this).attr('id'));
   })
   // mk 以上重構完成
-  $('#next-step').click(function(e) {
-    e.preventDefault();
-    const timeUnit = $('#time-unit');
-    timeUnit.show();
-    const top = timeUnit.offset().top;
-    $(window).scrollTop(top);
-  })
-  
-  $('#next-result').click(function(e) {
-    e.preventDefault();
-    const result = $('#result');
-    result.show();
-    const top = result.offset().top;
-    $(window).scrollTop(top);
-  })
-  
+
+  // 動畫按鈕綁定
   $('#level1').on('click', function() {
     $('#water-level').removeAttr("class")
     $('#water-level').addClass('level_1')
