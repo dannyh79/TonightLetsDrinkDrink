227f4da24adb36eb1c3618bd3e37638a1e227390
chaochaowu <chaowu.dev@gmail.com>
2019-06-27 20:07:57 +0800

    modified email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :weight, numericality: { greater_than: 0 }
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  # validates_uniqueness_of :email, message: '你的 Email 重複了'
 
   has_many :user_defines
 end
