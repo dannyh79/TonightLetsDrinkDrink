65f6d61efc222ce5cd80555016c584e36f389c53
Danny Cheng-Hsuan Han <30727089+dannyh79@users.noreply.github.com>
2019-06-28 10:37:33 +0800

    Unit test for Drink (#43)

--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Drink, type: :model do
-  # describe 'basics' do
-  #   it 'drinks have names, volume_alcohol, and img_path'
-  #   it 'drink\'s volume_alcohol is between 1 and 100'
-  # end
+  describe 'basics' do
+    it 'a drink has "name", "display_name", and "volume_alcohol"' do
+      should validate_presence_of(:name)
+      should validate_presence_of(:display_name)
+      should validate_presence_of(:volume_alcohol)
+    end
+    it 'drink\'s volume_alcohol is between 0 and 100' do
+      should validate_inclusion_of(:volume_alcohol).in_range(0..100)
+    end
+  end
 end
