7655a69bf96afacb2033de6d796b56f1a364b4d6
chaochaowu <chaowu.dev@gmail.com>
2019-07-01 14:01:13 +0800

    1. plus Google Oauth feature. 2. modified nav-bar if user sign in with google not show pwd-update button. 3. modified path because user need to fill in profile 4. delete pwd-confirmation column when user sign in with google. 5. modified updated method when user sign in with google.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,6 +263,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  
+  config.omniauth :google_oauth2, ENV["google_public_key"], ENV["google_private_key"],{access_type: "offline", approval_prompt: ""}  # omniauth.auth
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_034825) do
+ActiveRecord::Schema.define(version: 2019_06_30_140557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2019_06_17_034825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "display_name"
+  end
+
+  create_table "identities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "user_defines", force: :cascade do |t|
@@ -49,5 +58,6 @@ ActiveRecord::Schema.define(version: 2019_06_17_034825) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "identities", "users"
   add_foreign_key "user_defines", "users"
 end
