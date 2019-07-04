353f403eae2f2046cba7ec1a8b17a1ad2f41c448
chaochaowu <chaowu.dev@gmail.com>
2019-07-04 19:57:05 +0800

    fixed gender column

--- app/helpers/users/registration_helper.rb
+++ app/helpers/users/registration_helper.rb
@@ -5,5 +5,19 @@ module Users::RegistrationHelper
       sanitize 'checked="checked"'
     end
   end
+
+  def male?
+    if params[:user] != nil && params[:user][:gender] == "Male"
+      sanitize 'checked="checked"'
+    end
+  end
   
+  def female?
+    if params[:user] != nil && params[:user][:gender] == "Female"
+    sanitize 'checked="checked"'
+    end
+  end
+  
+
+
 end
