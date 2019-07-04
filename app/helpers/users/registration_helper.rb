89e1b477e0a740efb4d2c5f52e3e33d07f8af798
chaochaowu <chaowu.dev@gmail.com>
2019-07-04 19:08:01 +0800

    delete helper method

--- app/helpers/users/registration_helper.rb
+++ app/helpers/users/registration_helper.rb
@@ -5,12 +5,5 @@ module Users::RegistrationHelper
       sanitize 'checked="checked"'
     end
   end
-
-  def set_weight
-    # byebug
-    if params[:user].try(:[], :weight)
-      sanitize 'value="<%= params[:user][:weight] %>"'
-    end
-  end
   
 end
