64e9f10f1569bfe0e6d0b241950bfb232b9d3bf2
Chao.Wu <49583246+chaochaowu@users.noreply.github.com>
2019-07-04 12:06:16 +0800

    User spec - model (#66)
    * user spec
    
    * user spec reflector

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_defines
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :weight, numericality: { greater_than: 0 }
+  validates :email, :encrypted_password, :gender, :weight, presence: true
+
+  # email
   validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([a-zA-Z]{2,})\z/i
-  
-  has_many :user_defines
+  validates :email, uniqueness: true
+
+  # password
+  validates :password, length: { minimum: 6 }
+
+  # weight
+  validates :weight, numericality: { greater_than: 0 }
+
 end
