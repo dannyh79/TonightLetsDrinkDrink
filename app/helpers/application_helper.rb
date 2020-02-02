dc0ebe7f56c3dbf7218d6b95c674349d534d74fd
danny-ch han <chenghsuan@5xruby.tw>
2020-02-02 20:43:55 +0800

    Lint *.rb files in app/helpers/ (#91)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
-module ApplicationHelper
+# frozen_string_literal: true
 
+module ApplicationHelper
+  # FIXME: Refactor me
   def current_user_from_3rd_party_login?
     return false if current_user.nil?
+
     current_user.from_3rd_party_login?
   end
 end
