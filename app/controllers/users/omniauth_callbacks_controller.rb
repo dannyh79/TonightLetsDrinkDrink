ad1435b879dfc4244e2e135d6190efe3be912ff2
danny-ch han <chenghsuan@5xruby.tw>
2020-02-02 20:18:23 +0800

    Lint *_controller.rb (#91)

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,21 +1,19 @@
+# frozen_string_literal: true
+
+# FIXME: Unify sign-in & flash logic
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
-  # 向第三方請求開放授權
   def google_oauth2
-  
-    # 向第三方請求認證，key 在 /config/application.yml
     @user = User.from_omniauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = "認證成功"  # 原為 i18n 的 notice
+      flash[:notice] = "認證成功"
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url  # 設定 url 是給外部網站來做連結的
+      redirect_to new_user_registration_url
     end
   end
 
-  
   # 05 Jul., '19- Facebook would not whitelist our domain,
   # "https://tonight-lets-drink-drink.herokuapp.com", so the FB login
   # feature is taken out for now.
@@ -24,17 +22,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   @user = User.from_omniauth(request.env["omniauth.auth"])
 
   #   if @user.persisted?
-  #     sign_in_and_redirect @user, :event => :authentication
-  #     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+  #     sign_in_and_redirect @user, event: :authentication
+
+  #     return unless is_navigational_format?
+
+  #     set_flash_message(:notice, :success, kind: "Facebook")
   #   else
   #     session["devise.facebook_data"] = request.env["omniauth.auth"]
   #     redirect_to new_user_registration_url
   #   end
   # end
 
-  # 認證失敗
   def failure
     redirect_to new_user_session_path, alert: "無法取得認證！"
   end
-
 end
--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
+# TODO: Confirm whether the below is still relevant
 class Users::PasswordsController < Devise::PasswordsController
-
   # GET /resource/password/new
   # def new
   #   super
