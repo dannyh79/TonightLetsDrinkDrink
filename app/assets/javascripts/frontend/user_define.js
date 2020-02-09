6620f0825d56889633be2feff694b0813053c751
danny-ch han <chenghsuan@5xruby.tw>
2020-02-09 15:11:28 +0800

    Lint JS files (#94)

--- app/assets/javascripts/frontend/user_define.js
+++ app/assets/javascripts/frontend/user_define.js
@@ -1,6 +1,6 @@
-$(document).on('turbolinks:load', function () {
-    // 隱藏調酒內容區塊(如果沒有內容的話)
-    if ($('.calc-drinks-mini').children('.form-group').length > 0) {
-        $('.user_define-form').removeClass('d-none');
-    }
+$(document).on('turbolinks:load', () => {
+  // 隱藏調酒內容區塊(如果沒有內容的話)
+  if ($('.calc-drinks-mini').children('.form-group').length > 0) {
+    $('.user_define-form').removeClass('d-none');
+  }
 });
